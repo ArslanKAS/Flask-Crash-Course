962ae5141db4e188b8a18801d369f2a39db83776
ArslanKAS <arslanchaos@gmail.com>
2023-03-18 00:49:10 +0500

    Working

--- Blog_Site/server.py
+++ Blog_Site/server.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template
+import datetime
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def site_title():
-    return render_template("index.html")
+    year_current = datetime.date.today().year
+    return render_template("index.html", year = year_current)
 
+@app.route("/<name>")
+def name_guess(name):
+    guess = requests.get(f"https://api.agify.io?name={name}")
+    gender = requests.get(f"https://api.genderize.io?name={name}").json().get("gender")
+    name = guess.json().get("name")
+    age = guess.json().get("age")
+    return render_template(
+        "index.html",
+        username = name,
+        usergender = gender,
+        userage = age)
+
+# @app.route("/<name>")
+# def name_guess(name):
+#     guess = requests.get(f"https://api.agify.io?name={name}")
+#     name = guess.text("name")
+#     age = guess.text("age")
+#     return render_template("index.html", username = name, userage = age)
 
 if __name__ == "__main__":
     app.run(debug=True)
